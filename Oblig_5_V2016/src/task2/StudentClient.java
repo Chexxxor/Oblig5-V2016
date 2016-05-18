23a5ea0028ce275b0ef95270711c3c690d54cfe0
Kenneth Johan Kristensen <chexxor@outlook.com>
2016-05-18 09:03:05 +0200

    Added thread shutdown when shutdown is called for

--- Oblig_5_V2016/src/task2/StudentClient.java
+++ Oblig_5_V2016/src/task2/StudentClient.java
@@ -1,22 +1,23 @@
 package task2;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class StudentClient {
+	private ArrayList<Student> students = new ArrayList<>();
+	private boolean running = true;
+	
 	public static void main(String[] args){
 		new StudentClient();
 	}
 
 	public StudentClient() {
-		Scanner input = new Scanner(System.in);
 		try{
-			boolean running = true;
-			ArrayList<Student> students = new ArrayList<>();
-			students.add(new Student("Abe", "Lincoln", "Kings", "Washington", "DC", "85551"));
+			/*students.add(new Student("Abe", "Lincoln", "Kings", "Washington", "DC", "85551"));
 			students.add(new Student("Zacharry", "Bale", "Queens", "Chicago", "Wyoming", "81333"));
 			students.add(new Student("Bill", "Clinton", "Princeton", "Detroit", "Maine", "11942"));
 			students.add(new Student("Simon", "Bakeman", "Oxford", "Winchester", "Highlands", "37-123"));
@@ -25,25 +26,63 @@ public class StudentClient {
 			students.add(new Student("Tina", "Turner", "Broadway", "NYC", "NY", "88226"));
 			students.add(new Student("Hanna", "O'hanna", "Manwood", "Miami", "NY", "88302"));
 			students.add(new Student("Wilbur", "Mittens", "Elm street", "Liverpool", "Lowlands", "34-987"));
-			students.add(new Student("John", "Birch", "Queens", "NYC", "Massachusettes", "88223"));
+			students.add(new Student("John", "Birch", "Queens", "NYC", "Massachusettes", "88223"));*/
 			Socket socket = new Socket("localhost", 8000);
+			new Thread(new Listener(socket)).start();
 			ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-			for(Student s : students){
+			/*for(Student s : students){
 				out.writeObject(s);
-			}
+			}*/
+			Scanner input = new Scanner(System.in);
 			while(running){
-				Student s = newStudent(input);
-				out.writeObject(s);
+				Student s;
+				try {
+					s = newStudent(input);
+					out.writeObject(s);
+				} catch (ShutdownException e) {
+					running = false;
+				}
 			}
+			input.close();
 			socket.close();
 		}
 		catch(IOException e){}
-		input.close();
+	}
+	
+	private class Listener implements Runnable {
+		Socket socket;
+		
+		public Listener(Socket socket){
+			this.socket = socket;
+		}
+
+		@Override
+		public void run() {
+			System.out.println("Starting server listener...");
+			try {
+				ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+				System.out.println("Listener: Started");
+				while(running){
+					try {
+						Student s = (Student)in.readObject();
+						students.add(s);
+						System.out.println("Listener: Received object from server - " + s.toString());
+					} catch (ClassNotFoundException e) {
+						System.out.println("Listener: Bad data from server");
+					}
+				}
+			} catch (IOException e) {
+				running = false;
+				System.out.println("Listener: Failed to establish InputStream");
+			}
+		}
 	}
 
-	public Student newStudent(Scanner input){
-		System.out.print("First name: ");
+	public Student newStudent(Scanner input) throws ShutdownException {
+		System.out.println("First name (Type \"exit\" or \"close\" to stop client): ");
 		String fName = input.nextLine();
+		if(fName.toLowerCase().equals("exit") || fName.toLowerCase().equals("close"))
+			throw new ShutdownException();
 		System.out.print("Last name: ");
 		String lName = input.nextLine();
 		System.out.print("Street: ");
@@ -56,4 +95,7 @@ public class StudentClient {
 		String zip = input.nextLine();
 		return new Student(fName, lName, street, city, state, zip);
 	}
+	
+	@SuppressWarnings("serial")
+	private class ShutdownException extends Exception {}
 }
--- Oblig_5_V2016/src/task2/StudentServer.java
+++ Oblig_5_V2016/src/task2/StudentServer.java
@@ -6,6 +6,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InvalidClassException;
+import java.io.NotSerializableException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
@@ -18,83 +20,153 @@ public class StudentServer {
 	private boolean serverRunning = true;
 	private int count = 0;
 	private ArrayList<Student> students = new ArrayList<>();
+	private ArrayList<StudentThread> clients = new ArrayList<>();
+	ServerSocket serverSocket;
+	ConnectionListener connection;
 
 	public static void main(String[] args){
 		new StudentServer();
 	}
 
 	public StudentServer(){
-		loadFile();
+		System.out.println("Server starting...");
 		try{
-			ServerSocket serverSocket = new ServerSocket(8000);
-			System.out.println("Server started...");
+			loadFile();
+			serverSocket = new ServerSocket(8000);
+			System.out.println("Server started");
 
-			new Thread(() -> {
-				Scanner input = new Scanner(System.in);
-				while(serverRunning){
-					String command = input.nextLine();
-					switch(command){
-					case "exit":
-					case "close":
-					case "quit":
-						serverRunning = false;
-					default:
-						System.out.println(command);
-					}
-				}
-				input.close();
-			}).start();
-
-			while(count <= MAX_CLIENTS && serverRunning){
-				Socket socket = serverSocket.accept();
-				
-				new Thread(new StudentThread(socket, count++)).start();
-			}
+			InputListener listener = new InputListener();
+			listener.start();
+			(connection = new ConnectionListener()).start();
+			try {
+				listener.join();
+			} catch (InterruptedException e) {}
 			serverSocket.close();
+			saveFile();
 		}
 		catch(IOException e){
-			e.printStackTrace();
+			System.out.println("Server failed to start or close");
+		}
+	}
+	
+	private class ConnectionListener extends Thread {
+		@Override
+		public void run(){
+			while(clients.size() <= MAX_CLIENTS && serverRunning){
+				Socket socket;
+				try {
+					socket = serverSocket.accept();
+					StudentThread client = new StudentThread(socket, count++);
+					client.start();
+					clients.add(client);
+				} catch (IOException e) {
+					System.out.println("Server: Client connection failed");
+				}
+				
+			}
+		}
+	}
+	
+	private class InputListener extends Thread {
+		@Override
+		public void run(){
+			Scanner input = new Scanner(System.in);
+			while(serverRunning){
+				String command = input.nextLine();
+				switch(command){
+				case "exit":
+				case "close":
+				case "quit":
+					serverRunning = false;
+					System.out.println("Shutting down");
+				default:
+					System.out.println(command);
+				}
+			}
+			input.close();
+			if(connection.isAlive()){
+				try {
+					System.out.println("Waiting for client connections to shut down");
+					System.out.println("Forcing shutdown in 5 seconds");
+					sleep(5000);
+				} catch (InterruptedException e){}
+			}
+			System.out.println("Disconnecting clients");
+			for(StudentThread client : clients){
+				if(client.isAlive())
+					client.interrupt();
+			}
+			if(connection.isAlive()){
+				System.out.println("Shutting down connection");
+				connection.interrupt();
+			}
+			System.out.println("Shutting down input listener");
 		}
 	}
 
-	private class StudentThread implements Runnable {
+	private class StudentThread extends Thread {
 		private Socket socket;
 		private int number;
 		private boolean clientRunning = true;
-		private ObjectInputStream input;
+		private ObjectInputStream in;
+		private ObjectOutputStream out;
 
-		public StudentThread(Socket s, int n){
-			socket = s;
+		public StudentThread(Socket socket, int n){
+			this.socket = socket;
 			number = n;
+			try {
+				out = new ObjectOutputStream(socket.getOutputStream());
+				for(Student s : students){
+					send(s);
+				}
+			} catch (IOException e) {
+				clientRunning = false;
+				System.out.println("Client " + number + " - Failed establish OutputStream");
+			}
+		}
+		
+		public void send(Student s){
+			try {
+				out.writeObject(s);
+			} catch (IOException e) {
+				System.out.println("Client " + number + " - Failed to send object: " + s.toString());
+			}
 		}
 
 		public void run() {
 			try{
-				input = new ObjectInputStream(socket.getInputStream());
+				in = new ObjectInputStream(socket.getInputStream());
 				while(clientRunning && serverRunning){
 					try{
-						Student s = (Student)input.readObject();
+						Student s = (Student)in.readObject();
 						//System.out.println("Client " + number + " sent " + s.getName());
-						System.out.println("Client " + number + " sent " + s);
+						System.out.println("Client " + number + " sent " + s.toString());
 						students.add(s);
 						if(students.size() % 10 == 0)
 							saveFile();
+						for(StudentThread t : clients){
+							t.send(s);
+						}
 					}
-					catch(Exception e){
-						System.err.println(e);
+					catch(IOException e){
+						System.out.println("Client " + number + ": Connection failed");
 						clientRunning = false;
+					} catch (ClassNotFoundException e) {
+						System.out.println("Client " + number + ": Invalid data received");
 					}
 				}
-				input.close();
-			}
-			catch(IOException e){
-				System.err.println(e);
+				in.close();
+			} catch(IOException e){
+				clientRunning = false;
+				System.out.println("Client " + number + " - Failed to establish InputStream");
+			} finally {
+				clients.remove(this);
 			}
 		}
 	}
 
 	public void loadFile(){
-		System.out.println("Starting Load");
+		System.out.println("Starting Load...");
 		File f = new File("Students.txt");
 		try {
 			boolean continueReading = true;
@@ -111,30 +183,40 @@ public class StudentServer {
 				}
 			}
 			in.close();
-			System.out.println("Load complete - loaded " + count + " objects:");
+			System.out.println("Load complete - Loaded " + count + " objects:");
 		} catch(FileNotFoundException e) {
 			System.out.println("Loading failed - File not found");
 		} catch(IOException e) {
 			System.out.println("Loading failed - Failed to establish InputStream");
 		} catch(ClassNotFoundException e){
-			System.out.println("Loading failed - invalid class type received");
+			System.out.println("Loading failed - Invalid object type received");
 		}
 	}
 
 	public void saveFile(){
-		System.out.println("Starting Save");
+		System.out.println("Starting save...");
 		File f = new File("Students.txt");
-		try {
-			int count = 0;
+		try{
 			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(f));
+			int count = 0;
 			for(Student s : students){
 				count++;
-				out.writeObject(s);
+				try{
+					out.writeObject(s);
+				} catch(NotSerializableException | InvalidClassException e) {
+					System.out.println("Failed to save object: " + s.toString() + " - Bad object type");
+				} catch(IOException e) {
+					System.out.println("Failed to save object: " + s.toString() + " - Bad OutputStream");
+				}
 			}
-			System.out.println("Saved " + count + " objects.");
-			out.close();
-		} catch (Exception e) {
-			e.printStackTrace();
+			System.out.println("Save complete - Saved " + count + " objects.");
+			try{
+				out.close();
+			} catch(IOException e){
+				System.out.println("Error closing file during save");
+			}
+		} catch(IOException e) {
+			System.out.println("Save failed - Failed to establish OutputStream");
 		}
 	}
 }
